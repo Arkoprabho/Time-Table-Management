1c38c6ac5e4e63863bcc7f85cc0fa7735b1c3fdc
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-01-11 00:59:30 +0530

    Added some basic functionalities

---
+++ src/com/ard/oosd/a/MainClass.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.ard.oosd.a;
+
+/**
+ * @author Arko
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 * This class is only for my personal testing purpose.
+	 * TODO remove once there is a different parth of execution
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student = new Student();
+		student.input();
+	}
+
+}
---
+++ src/com/ard/oosd/a/Student.java
@@ -0,0 +1,44 @@
+/**
+ * company.arkorishabhdavid.objectorientedsystemdesign.arko
+ * Contains code that was created by arko
+ */
+package com.ard.oosd.a;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Arko<br>
+ * Contains the basic functionalities of a student
+ */
+class Student implements People{
+	// The student can login, view his time table.
+	int rollNumber;
+	/**
+	 * Takes the input from the user.
+	 */
+	void input(){
+		// TODO needs to be changed when the GUI is fixed
+		System.out.print("Input roll number: ");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			rollNumber = Integer.parseInt(reader.readLine());
+			System.out.println(rollNumber);
+		}
+		catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void views() {
+		// TODO implement the interface methods
+		
+	}
+	@Override
+	public void login() {
+		// TODO implement the interface methods
+		
+	}
+}
