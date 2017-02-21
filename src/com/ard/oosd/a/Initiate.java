4e0b7541da672341049a2307d9dfbe01e79d9b27
arko <arko@arko-HP-ProBook-440-G2>
2017-02-21 13:27:46 +0530

    Made sure no 2 subjects with same code can be added

--- src/com/ard/oosd/a/Initiate.java
+++ src/com/ard/oosd/a/Initiate.java
@@ -6,8 +6,8 @@ package com.ard.oosd.a;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 /**
@@ -16,11 +16,9 @@ import java.util.List;
  */
 class Initiate {
     // Variable declaration
-    String name = null;
-    ArrayList<Subjects> subjectsAssociated = null;
-    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    private BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+    private ConcurrentHashMap<Professor, HashSet<Subjects>> professorHashMap= new ConcurrentHashMap<>();
     
-    List<Professor> professorList = new ArrayList<>();
     
     /**
      * Gets the input from the user.
@@ -28,45 +26,36 @@ class Initiate {
      */
     void input() throws IOException {
 	
-	subjectsAssociated = new ArrayList<Subjects>(3);
+	int numberOfSubjects;
+	String name;
+	HashSet<Subjects> subjectList = new HashSet<>();
+	
 	System.out.print("Enter the name of the professor: ");
-	name = input.readLine();
+	name = inputReader.readLine();
+	System.out.print("How many subjects are associated with this:  ");
+	numberOfSubjects = Integer.parseInt(inputReader.readLine());
 	
-	System.out.println("Here is the coding of the subjects: ");
-	
-	// Display the subjects so that the user can enter the correct code.
-	for(Subjects s : Subjects.values()) {
-	    System.out.println("Subject name: "+s+"Subject code: "+s.getValue());
-	}
-	System.out.print("Enter the codes of the subjects he is associated with(3): ");
-	
-	// Get the code from the user and feed it to the subject associated list.
-	// TODO: if the code doest not match then it is completely ignored. This behaviour needs to be fixed, so that on entering a wrong code, it should throw an error and let the user enter it again.
-	for (int i = 0; i < 3; i++) {
-	    int subCode = Integer.parseInt(input.readLine());
-	    for(Subjects s : Subjects.values()) {
-		if(subCode == s.getValue()){
-		    subjectsAssociated.add(s);
-		}
+	// Create the list of subject that the professor can teach.
+	while(numberOfSubjects-- != 0) {
+	    String subName;
+	    int code, credit; 
+	    System.out.print("Subject name: ");
+	    subName = inputReader.readLine();
+	    System.out.print("Subject code: ");
+	    code = Integer.parseInt(inputReader.readLine());
+	    System.out.print("Subject credit: ");
+	    credit = Integer.parseInt(inputReader.readLine());
+	    Subjects subjectToAdd = new Subjects(subName, code, credit);
+	    if(!Subjects.CheckEquality(subjectToAdd, subjectList)) {
+		System.out.println("Error. Try again!");
+		numberOfSubjects++;
 	    }
+	    else
+		subjectList.add(subjectToAdd);
+	    
 	}
-	// Add the details to a list of professor.
-	professorList.add(new Professor(name, subjectsAssociated));
-    }
-    
-    /**
-     * Displays the output to standard output console.
-     */
-    void output() {
-	// Display the list of the professor and its associated subjects.
-	for (Professor professor : professorList) {
-	    System.out.println("Professor name: "+professor.getProfessorname());
-	    System.out.println("Subjects associated: ");
-	    for (Subjects s: professor.getAssociatedSubjects()) {
-		System.out.println(s.toString());
-	    }
-	}
-	// Enter an extra line for aesthetics sake.
-	System.out.println();
+	
+	// Associate the subjects with the professor.
+	professorHashMap.put(new Professor(name), subjectList);
     }
 }
