8756a81d1e396c100aea4bd1b55a36d5c11b20e5
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-03-10 22:54:13 +0530

    added a method to perform initial checks

--- src/com/ard/oosd/a/MainClass.java
+++ src/com/ard/oosd/a/MainClass.java
@@ -1,34 +1,43 @@
-/**
- * 
- */
 package com.ard.oosd.a;
 
 import java.awt.EventQueue;
 
+import com.ard.oosd.a.sqlscripts.DatabaseConnection;
 import com.ard.oosd.r.WindowMain;
 
 /**
  * @author Arko
- *
+ * Acts as the starting point of the program.
  */
 public class MainClass {
 
     /**
-     * @param args
-     * This class is only for my personal testing purpose.
-     * TODO remove once there is a different parth of execution
+     * Perform the initial checks before starting the program.
+     * Make sure that the program is ready to be executed.
      */
-    public static void main(String[] args) {
-	EventQueue.invokeLater(new Runnable() {
-	    public void run() {
-		try {
-		    WindowMain window = new WindowMain();
-		    window.frame.setVisible(true);
-		} catch (Exception e) {
-		    e.printStackTrace();
-		}
-	    }
-	});
+    private void performInitCheck() {
+        // TODO perform the required checks.
+        /*
+        Checks:
+        1. Does the necessary databases exist (timetablemanagement)
+            1.1 If not create it.
+        2. Is the timetable updated? Compare the update time of the table and the dump file.
+         */
+        new DatabaseConnection();
     }
 
+    /**
+     * Main method. Starting point of the program.
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+    	new MainClass().performInitCheck();
+		EventQueue.invokeLater(() -> {
+	    	try {
+		    	new WindowMain().frame.setVisible(true);
+		    } catch (Exception e) {
+		        e.printStackTrace();
+		    }
+	    });
+    }
 }
