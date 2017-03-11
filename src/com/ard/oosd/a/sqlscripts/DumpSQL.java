540ab51be554ff095c5e2ef97ea567d298dcd4ea
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-03-11 08:25:01 +0530

    Created DumpSQL to dump mysql files

---
+++ src/com/ard/oosd/a/sqlscripts/DumpSQL.java
@@ -0,0 +1,19 @@
+package com.ard.oosd.a.sqlscripts;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+
+/**
+ * Contains methods to create a dump file of the SQL Schema
+ * Created by arko on 11-03-2017.
+ */
+public class DumpSQL {
+    /**
+     * @return the last modified time of the mysqldump file
+     */
+    private String dumpFileModifiedTime() {
+        File file = new File("src/com/ard/oosd/a/sqlscripts/scripts/Time-Table-Management-Dump.sql");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+        return simpleDateFormat.format(file.lastModified());
+    }
+}
