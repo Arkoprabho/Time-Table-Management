414522292d26e4ae264379161df9bf870dfe598c
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-03-14 21:05:22 +0530

    Created necessary methods to backup database.

--- src/com/ard/oosd/a/sqlscripts/DumpSQL.java
+++ src/com/ard/oosd/a/sqlscripts/DumpSQL.java
@@ -1,14 +1,30 @@
 package com.ard.oosd.a.sqlscripts;
 
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.text.SimpleDateFormat;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Contains methods to create a dump file of the SQL Schema
  * Created by arko on 11-03-2017.
  */
 public class DumpSQL {
+    static Logger LOGGER = null;
+    FileHandler logFile = null;
+
+    public DumpSQL() {
+         LOGGER = Logger.getLogger(Class.class.getName());
+        try {
+            logFile = new FileHandler("log.log");
+            LOGGER.addHandler(logFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * @return the last modified time of the mysqldump file
@@ -19,7 +35,42 @@ public class DumpSQL {
         return simpleDateFormat.format(file.lastModified());
     }
 
-    void updateDump(Connection connection) {
+    /**
+     * Creates a mysqldump of the database.
+     * @param userName username of the database.
+     * @param password password of the database.
+     * @param tableName name of the table to dump.
+     * @throws IOException In case it is unable to execute the command
+     * @throws InterruptedException Wait fails
+     */
+    void backupDatabase(String userName, String password, String tableName) throws IOException, InterruptedException {
+        String executeCommand = "mysqldump -u "+userName+" -p"+password+"-r Time-Table-Management-Dump.sql";
+        Process process = Runtime.getRuntime().exec(executeCommand);
+        int processCompletion = process.waitFor();
+        if(processCompletion == 0)
+            LOGGER.log(Level.INFO, "Backup success!");
+        else{
+            LOGGER.log(Level.SEVERE, "Backup failed!");
+            System.out.println("BACKUP FAILED!");
+        }
+    }
 
+    /**
+     * Restores the database with the help of the dump that was created.
+     * @param userName username of the database.
+     * @param password password of the database.
+     * @throws IOException In case it is unable to execute the command
+     * @throws InterruptedException Wait fails
+     */
+    void restoreDatabase(String userName, String password) throws IOException, InterruptedException {
+        String executeCommand = "mysql -u"+userName+"-p"+password+"< Time-Table-Management-Dump.sql";
+        Process process = Runtime.getRuntime().exec(executeCommand);
+        int processCompletion = process.waitFor();
+        if(processCompletion == 0)
+            LOGGER.log(Level.INFO, "Restore success!");
+        else{
+            LOGGER.log(Level.SEVERE, "Restore failed!");
+            System.out.println("RESTORE FAILED!");
+        }
     }
 }
