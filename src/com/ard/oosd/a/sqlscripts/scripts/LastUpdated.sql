9568a64758847cbaece4e409ef6b63e9d0b5d931
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-03-14 19:34:01 +0530

    Removed unnecessary files

--- src/com/ard/oosd/a/sqlscripts/scripts/LastUpdated.sql
+++
@@ -1,9 +0,0 @@
-# noinspection SqlResolveForFile
-
-/*
-Checks the last updated time of the database.
-*/
-SELECT UPDATE_TIME
-FROM   information_schema.tables
-WHERE  TABLE_SCHEMA = 'timetablemanagement'
-       AND TABLE_NAME = 'abc'
