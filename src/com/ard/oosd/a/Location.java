db5b74f12650b8dbb364b7cac8d0049a6929efcc
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-03-24 04:02:02 +0530

    Created file to specify location of lecture

---
+++ src/com/ard/oosd/a/Location.java
@@ -0,0 +1,41 @@
+package com.ard.oosd.a;
+
+/**
+ * Contains methods to define the locations like the room number.
+ */
+public class Location {
+    private String roomName;
+    private int roomHashCode;
+
+    /**
+     * @return the room number of the current object.
+     */
+    public String getRoomName() {
+        return roomName;
+    }
+
+    /**
+     * Sets the room number for the current object.\n
+     * @param value the value of the room.
+     */
+    void setRoomName(String value) {
+        roomName = value;
+        roomHashCode = this.hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 29;
+        hash = 92 * hash + (this.roomName != null ? this.roomName.toLowerCase().hashCode() : 0);
+        return hash;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == null)
+            return false;
+        Location other = (Location) o;
+        return other.roomHashCode == this.hashCode();
+    }
+}
