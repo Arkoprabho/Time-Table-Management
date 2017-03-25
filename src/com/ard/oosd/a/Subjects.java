6503b0df080117c4200fe04528b171df5c977b04
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-03-25 05:30:18 +0530

    Removed the CheckEquality method as it is automatically done by the database.

--- src/com/ard/oosd/a/Subjects.java
+++ src/com/ard/oosd/a/Subjects.java
@@ -44,35 +44,4 @@ class Subjects {
     public String getSubjectCode() {
         return subjectCode;
     }
-
-    /**
-     * Check if the subject has the same code as any other subject in the list.
-     * @param subjectList the list of subjects.
-     * @return checks if the subject already exists in the list.
-     */
-    public boolean CheckEquality(List<Subjects> subjectList) {
-        for (Subjects subjects : subjectList) {
-            if(subjects.equals(this))
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + credit;
-        result = prime * result + subjectCode.hashCode();
-        result = prime * result + ((subjectName == null) ? 0 : subjectName.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if(obj == null)
-            return false;
-        Subjects other = (Subjects) obj;
-        return Objects.equals(subjectCode, other.subjectCode);
-    }
 }
