d5e661befe40dee85025689cbea5d2d6ac9c1025
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-04-02 22:11:32 +0530

    Used cmd to execute mysql script

--- src/com/ard/oosd/a/sqlscripts/DumpSQL.java
+++ src/com/ard/oosd/a/sqlscripts/DumpSQL.java
@@ -4,15 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Contains methods to create a dump file of the SQL Schema
  * Created by arko on 11-03-2017.
  */
-public class DumpSQL {
+class DumpSQL {
 
     /**
      * @return the last modified time of the mysqldump file
@@ -33,7 +30,7 @@ public class DumpSQL {
      */
     void backupDatabase(String userName, String password, String databaseName) throws IOException, InterruptedException {
         //NOTE  Make sure mysql is added to PATH
-        String[] executeCommand = new String[] {"cmd.exe", "/c", "\"C:/Program Files (x86)/MySQL/MySQL Server 5.7/bin/mysqldump.exe\" -u "+userName+" -p"+password+" --databases "+databaseName+" > src/com/ard/oosd/a/sqlscripts/scripts/Time-Table-Management-Dump.sql"};
+        String[] executeCommand = new String[] {"cmd.exe", "/c", "\"C:/Program Files (x86)/MySQL/MySQL Server 5.7/bin/mysqldump.exe\" --compact --add-drop-database -u "+userName+" -p"+password+" -B "+databaseName+" > Time-Table-Management-Dump.sql"};
         Process process = Runtime.getRuntime().exec(executeCommand);
         if(process.waitFor() == 0) {
             InputStream inputStream = process.getInputStream();
@@ -54,19 +51,33 @@ public class DumpSQL {
 
     /**
      * Restores the database with the help of the dump that was created.
-     * @param userName username of the database.
-     * @param password password of the database.
-     * @throws IOException In case it is unable to execute the command
-     * @throws InterruptedException Wait fails
      */
-    void restoreDatabase(String userName, String password) throws IOException, InterruptedException {
-        String executeCommand = "mysql -u"+userName+"-p"+password+"< Time-Table-Management-Dump.sql";
-        Process process = Runtime.getRuntime().exec(executeCommand);
-        int processCompletion = process.waitFor();
-        if(processCompletion == 0)
-            System.out.println("RESTORE SUCCESS!");
-        else{
-            System.out.println("RESTORE FAILED!");
+    void restoreDatabase() {
+        try{
+            String[] executeCommand = new String[] {
+                    "cmd.exe",
+                    "/c",
+                    "mysql -u"+DatabaseConnection.USER+" -p"+DatabaseConnection.PASSWORD+" -B "+DatabaseConnection.DATABASE+" < \"Time-Table-Management-Dump.sql\""
+            };
+            Process process = Runtime.getRuntime().exec(executeCommand);
+            if(process.waitFor() == 0) {
+                InputStream inputStream = process.getInputStream();
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+                String out = new String(buffer);
+                System.out.println(out);
+            }
+            else {
+                System.out.println(process.waitFor());
+                InputStream errorStream = process.getErrorStream();
+                byte[] buffer = new byte[errorStream.available()];
+                errorStream.read(buffer);
+                String out = new String(buffer);
+                System.out.println(out);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
