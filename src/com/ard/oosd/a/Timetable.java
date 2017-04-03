323a739019b8fd5d634fed087ae2b5cf31a9dfe9
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-04-03 07:42:14 +0530

    Fixed time table generation

--- src/com/ard/oosd/a/Timetable.java
+++ src/com/ard/oosd/a/Timetable.java
@@ -1,7 +1,6 @@
 package com.ard.oosd.a;
 
-import org.cpsolver.ifs.example.tt.TimetableModel;
-
+import org.cpsolver.coursett.Test;
 /**
  * Contains methods to generate the time table and add it to the database.
  */
@@ -9,12 +8,13 @@ public class Timetable {
 
     /**
      * Generates the time table with the constraints.
+     * @param pathToOutputFolder the path to the output folder where the final soluction will reside.
      * @param pathToXMLFile the path to the XML file that contains details of the constraints
      */
-    void generateTimeTable(String pathToXMLFile) {
-        TimetableModel.main(new String[] {
+    void generateTimeTable(String pathToXMLFile, String pathToOutputFolder) {
+        Test.main(new String[] {
                 pathToXMLFile,
-                "src/generatedTimeTable.xml"
+                pathToOutputFolder
         });
     }
 }
