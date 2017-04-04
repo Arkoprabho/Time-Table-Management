f1462b9cbc21c64e63ee183b41bb9138785b10da
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-04-04 19:14:41 +0530

    Will now write to database everytime an object is initialized

--- src/com/ard/oosd/a/Professor.java
+++ src/com/ard/oosd/a/Professor.java
@@ -3,19 +3,22 @@
  */
 package com.ard.oosd.a;
 
+import com.ard.oosd.a.sqlscripts.DatabaseConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Contains details of the professor.
  * @author Arko
  *
  */
-class Professor {
+class Professor implements DatabaseEntryInterface {
     // Private fields
     private String professorName = null;
+    private String tableName = "faculty";
     private List<Subjects> associatedSubject = new ArrayList<>();
     
     /**
@@ -28,20 +31,22 @@ class Professor {
 	    professorName = name;
 	    // Set the associated subjects with the professor.
         setAssociatedSubject(subjects);
+        // Write the details to database for fetch later on.
+        writeToDatabase();
     }
 
     /**
      * @return the professorname
      */
     public String getProfessorname() {
-	return professorName;
+	    return professorName;
     }
 
     /**
      * @return the associatedSubject
      */
     public List<Subjects> getAssociatedSubject() {
-	return associatedSubject;
+	    return associatedSubject;
     }
 
     /**
@@ -51,4 +56,30 @@ class Professor {
 	this.associatedSubject = associatedSubject;
     }
 
+    /**
+     * Writes the data about the object to database
+     *
+     * @return true if write is successful
+     */
+    @Override
+    public boolean writeToDatabase() {
+        // INSERT INTO FACULTY (NAME, SUBJECTCODE, TYPE) VALUES (getProfessorname(), , Teacher)
+        try (PreparedStatement facultyStatement = DatabaseConnection.connection.prepareStatement("INSERT INTO ? (?, ?, Type) VALUES (?, ?, Teacher)")) {
+            facultyStatement.setString(1, this.tableName);
+            facultyStatement.setString(2, "Name");
+            facultyStatement.setString(3, "SubjectCode");
+            facultyStatement.setString(4, this.getProfessorname());
+            // Make sure that the subject also exists in the database.
+            for (Subjects subject : associatedSubject) {
+                // Might throw an exception in case the Subject already exists in the database.
+                subject.writeToDatabase();
+                facultyStatement.setString(5, subject.getSubjectCode());
+            }
+            facultyStatement.execute();
+        } catch (SQLException e) {
+            // In case any of the entries are not executed.
+            return false;
+        }
+        return true;
+    }
 }
