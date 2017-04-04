b2c654ba34f028adc6663fd3a8f836c5f5c2e67b
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-04-04 19:15:39 +0530

    Added method to write to database

--- src/com/ard/oosd/a/Subjects.java
+++ src/com/ard/oosd/a/Subjects.java
@@ -3,35 +3,39 @@
  */
 package com.ard.oosd.a;
 
-import java.util.List;
+import com.ard.oosd.a.sqlscripts.DatabaseConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Class that holds the subjects associated with a professor.
  * @author Arko
  */
-class Subjects {
+class Subjects implements DatabaseEntryInterface {
     // Each subject needs to have a subject code, name and credit.
-    int credit;
-    String subjectName, subjectCode;
+    private int credit;
+    private String subjectName, subjectCode;
 
     /**
      * Initializes a new instance of a subject.
-     * @param credit
-     * @param name
+     * @param code subject code
+     * @param credit credit of the subject
+     * @param name name of the subject
      */
     public Subjects(String name, String code, int credit) {
-	subjectCode = code;
-	this.credit = credit;
-	subjectName = name;
+        subjectCode = code;
+        this.credit = credit;
+        subjectName = name;
     }
 
     @Override
     public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + credit;
-	result = prime * result + ((subjectName == null) ? 0 : subjectName.hashCode());
-	return result;
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + credit;
+        result = prime * result + ((subjectName == null) ? 0 : subjectName.hashCode());
+        return result;
     }
 
     @Override
@@ -39,4 +43,47 @@ class Subjects {
 		Subjects other = (Subjects) obj;
 		return subjectCode == other.subjectCode;
 	}
+
+    /**
+     * Writes the data about the object to database
+     *
+     * @return true if write is successful
+     */
+    @Override
+    public boolean writeToDatabase() {
+        try(PreparedStatement studentStatement = DatabaseConnection.connection.prepareStatement("INSERT INTO ? (?, ?, ?) VALUES (?, ?, ?)")) {
+
+            studentStatement.setString(1, "subjects");
+            studentStatement.setString(2, this.getSubjectCode());
+            studentStatement.setString(3, this.getSubjectName());
+            studentStatement.setInt(4, this.getCredit());
+
+            studentStatement.execute();
+        } catch (SQLException e) {
+            if(e.getErrorCode() == 1062)
+                System.out.println("Subject code already exists!");
+        }
+        return false;
+    }
+
+    /**
+     * @return the credit of the subject
+     */
+    private int getCredit() {
+        return credit;
+    }
+
+    /**
+     * @return the name of the subject
+     */
+    private String getSubjectName() {
+        return subjectName;
+    }
+
+    /**
+     * @return SubjectCode
+     */
+    String getSubjectCode() {
+        return subjectCode;
+    }
 }
