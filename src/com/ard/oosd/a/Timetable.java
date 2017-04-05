34bd756ded85d8b38f9a4ba6c2bf6d800c218e68
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2017-04-05 11:30:46 +0530

    Generates time table on a new thread

--- src/com/ard/oosd/a/Timetable.java
+++ src/com/ard/oosd/a/Timetable.java
@@ -1,22 +1,29 @@
 package com.ard.oosd.a;
 
 import org.cpsolver.coursett.Test;
+
 /**
+ * Contains methods to generate time table
  * Created by arko on 03-04-2017.
  */
-public class Timetable {
+public class Timetable implements Runnable {
     /**
      * Generates the time table using the cpsolver API
      */
     public void generate() {
         String configPath, inputPath, outputFolderPath;
-        configPath = "llr.cfg";
-        inputPath = "input.xml";
+        configPath = "src/llr.cfg";
+        inputPath = "src/input.xml";
         outputFolderPath = "./src/output";
         Test.main(new String[] {
                 configPath,
                 inputPath,
                 outputFolderPath
         });
+    }
+
+    @Override
+    public void run() {
+        generate();
     }
 }
